8c2bc0b8a1210758b13f3f6ec83f3e03629afc6f
manjunath-dalavaye <manjunathadvg2002@gmail.com>
2023-12-27 18:19:28 +0530

    firsty

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -1,79 +1,48 @@
-// import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-// import Tooltip from "@material-ui/core/Tooltip";
-// import Chip from "@material-ui/core/Chip";
-// import { Link } from "react-router-dom";
-// import style from "./Card.module.css";
-
-
-
 import React from "react";
-import Tooltip from "@mui/material/Tooltip";
-import Chip from "@mui/material/Chip";
+import styles from "./Card.module.css";
+import { Chip, Tooltip } from "@mui/material";
 import { Link } from "react-router-dom";
-import { styled } from "@mui/system";
-import style from "./Card.module.css";
-
-
-const CustomTooltip = withStyles((theme) => ({
-  tooltip: {
-    backgroundColor: "black" , // Change to your desired tooltip background color
-    color: "#34C94B",
-    fontSize: theme.typography.pxToRem(13),
-    border: "1px solid #dadde9",
-  },
-}))(Tooltip);
-
-// const CustomTooltip = styled(Tooltip)({
-//   backgroundColor: "black",
-//   color: "#34C94B",
-//   fontSize: (theme) => theme.typography.pxToRem(13),
-//   border: "1px solid #dadde9",
-// });
 
 function Card({ data, type }) {
   const getCard = (type) => {
     switch (type) {
       case "album": {
-        const { image, title, follows, slug, songs } = data;
+        const { image, title, follows, songs, slug } = data;
         return (
-          <CustomTooltip title={`${songs.length} songs`} placement="top" arrow
-            enterTouchDelay={0}
-            leaveTouchDelay={600}
-          >
+          <Tooltip title={`${songs.length} songs`} placement="top" arrow>
             <Link to={`/album/${slug}`}>
-              <div className={style.Wrapper}>
-                <div className={style.card}>
+              <div className={styles.wrapper}>
+                <div className={styles.card}>
                   <img src={image} alt="song" loading="lazy" />
-                  <div className={style.banner}>
+                  <div className={styles.banner}>
                     <Chip
                       label={`${follows} Follows`}
                       size="small"
-                      className={style.chip}
+                      className={styles.chip}
                     />
                   </div>
                 </div>
-                <div className={style.titleWrapper}>
+                <div className={styles.titleWrapper}>
                   <p>{title}</p>
                 </div>
               </div>
             </Link>
-          </CustomTooltip>
+          </Tooltip>
         );
       }
       case "song": {
-        const { likes, image, title } = data;
+        const { image, likes, title } = data;
         return (
-          <div className={style.Wrapper}>
-            <div className={style.card}>
+          <div className={styles.wrapper}>
+            <div className={styles.card}>
               <img src={image} alt="song" loading="lazy" />
-              <div className={style.banner}>
-                <div className={style.pill}>
+              <div className={styles.banner}>
+                <div className={styles.pill}>
                   <p>{likes} Likes</p>
                 </div>
               </div>
             </div>
-            <div className={style.titleWrapper}>
+            <div className={styles.titleWrapper}>
               <p>{title}</p>
             </div>
           </div>
@@ -83,7 +52,6 @@ function Card({ data, type }) {
         return <></>;
     }
   };
-
   return getCard(type);
 }
 
