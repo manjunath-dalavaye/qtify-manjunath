13ec7b499fef3b294939c6c68dc6b466555c2a94
manjunath-dalavaye <manjunathadvg2002@gmail.com>
2023-12-27 20:25:39 +0530

    caro

--- src/component/Section/Section.jsx
+++ src/component/Section/Section.jsx
@@ -3,40 +3,40 @@ import React, { useEffect, useState } from "react";
 
 import styles from "./Section.module.css";
 import { CircularProgress } from "@mui/material";
-// import Carousel from "../Carousel/Carousel";
-// import Filters from "../Filters/Filters";
+import Carousel from "../Carousel/Carousel";
+import Filters from "../Filters/Filters";
 
 
-function Section({ title, data, type}) {
-    // const [filters, setFilters] = useState([{ key: "all", label: "All"}])
-    // const [selectedFilterIndex, setSelectedFilterIndex] = useState(0);
-    const [carouselToggle, setCarouselToggle] = useState(false);
+export default function Section({ title, data, filterSource, type}) {
+    const [filters, setFilters] = useState([{ key: "all", label: "All"}])
+    const [selectedFilterIndex, setSelectedFilterIndex] = useState(0);
+    const [carouselToggle, setCarouselToggle] = useState(true);
 
     const handleToggle = () => {
         setCarouselToggle((prevState) => !prevState);
     }
-    // useEffect(() => {
-    //     if (filterSource) {
-    //         filterSource().then((response) => {
-    //             const {data} = response;
-    //             setFilters([...filters, ...data]);
-    //         });
-    //     }
-    // }, []);
-    // const showFilters = filters.length > 1;
-    // const cardsToRender = data.filter((card) =>
-    //     showFilters && selectedFilterIndex !== 0
-    //         ? card.genre.key === filters[selectedFilterIndex].key
-    //         : card);
+    useEffect(() => {
+        if (filterSource) {
+            filterSource().then((response) => {
+                const {data} = response;
+                setFilters([...filters, ...data]);
+            });
+        }
+    }, []);
+    const showFilters = filters.length > 1;
+    const cardsToRender = data.filter((card) =>
+        showFilters && selectedFilterIndex !== 0
+            ? card.genre.key === filters[selectedFilterIndex].key
+            : card);
 
 
     return (
         <div>
             <div className={styles.header}>
                 <h3>{title}</h3>
-                <h4 className={styles.toggleText} onClick={handleToggle}>{!carouselToggle ? "Collapse All": "Show All"}</h4>
+                {!showFilters && (<h4 className={styles.toggleText} onClick={handleToggle}>{!carouselToggle ? "Collapse All": "Show All"}</h4>)}
             </div>
-            {/* {showFilters && (
+            {showFilters && (
                 <div className={styles.filterWrapper}>
                     <Filters
                         filters={filters}
@@ -44,28 +44,25 @@ function Section({ title, data, type}) {
                         setSelectedFilterIndex={setSelectedFilterIndex}
                     />
                 </div>
-             )} */}
-              {data.length === 0 ? (
+            )}
+            {data.length === 0 ? (
                 <CircularProgress />
-             ): (
+            ): (
                 <div className={styles.cardWrapper}>
                     {!carouselToggle ? (
                         <div className={styles.wrapper}>
-                            {data.map((ele) => (
+                            {cardsToRender.map((ele) => (
                                 <Card data={ele} type={type} />
                             ))}
                         </div>
                     ):(
-                        // <Carousel
-                        //     data={cardsToRender}
-                        //     renderComponent={(data) => <Card data={data} type={type} />}
-                        // />
-                        null
+                        <Carousel
+                            data={cardsToRender}
+                            renderComponent={(data) => <Card data={data} type={type} />}
+                        />
                     )}
                 </div>
             )}
         </div>
     )
 }
-
-export default Section;
